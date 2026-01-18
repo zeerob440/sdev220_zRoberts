776f3808fcf67d5728eeb51562e6c229782f7151
zeerob440 <zeerob440@tutanota.com>
2026-01-18 18:39:42 -0500

    fix me: determine path forward.

--- mod2Lab.py
+++ mod2Lab.py
@@ -1,8 +1,17 @@
 '''
-Module 2 Lab - Case Study: if...else and while
+mod2Lab.py
 Proudly Engineered by Zachary Roberts 18 JAN 2026.
 
 This program allows the user to input names and GPAs and returns if a student has achieved Honor Roll or Dean's list.
 
-
 '''
+
+# create while loop to accept students first name and last name.
+# FIXME Determine path forward with this. classes seem an option for this use case, or maybe a dict... Will think on this over night. 
+
+class studentRecord:
+    def __int__(self, fname, lname, gpa):
+        self.fname = fname 
+        self.lname = lname
+        self.gpa = gpa
+
