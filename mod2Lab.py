012448b8a7cd5ad7ede1e5e27e644ca7d822b256
zeerob440 <zeerob440@tutanota.com>
2026-01-18 17:45:32 -0500

    feat: mod2Lab.py created.

---
+++ mod2Lab.py
@@ -0,0 +1,8 @@
+'''
+Module 2 Lab - Case Study: if...else and while
+Proudly Engineered by Zachary Roberts 18 JAN 2026.
+
+This program allows the user to input names and GPAs and returns if a student has achieved Honor Roll or Dean's list.
+
+
+'''
