520e5f3724dac6f98dd7fe7946b1c79fa6f64a8b
zeerob440 <zeerob440@tutanota.com>
2026-01-19 12:04:54 -0500

    feat: input validation added to gpa. Not added to first_name or last_name, I suppose a numeric could be a name.

--- mod2Lab.py
+++ mod2Lab.py
@@ -8,7 +8,7 @@ Specifications:
 Write a Python app that will accept student names and GPAs and test if the student qualifies for either the Dean's List or the Honor Roll. Your app will:
 ask for and accept a student's last name.
 quit processing student records if the last name entered is 'ZZZ'.
-ask for and accept a student's first name.
+ask for and accept a student's first name. 
 ask for and accept the student's GPA as a float.
 test if the student's GPA is 3.5 or greater and, if so, print a message saying that the student has made the Dean's List.
 test if the student's GPA is 3.25 or greater and, if so, print a message saying that the student has made the Honor Roll.
@@ -20,27 +20,37 @@ A brief description of what your app will do
 '''
 
 ''' Schema
-# FIXME Determine path forward with this. I have written a similar program in SDEV-120 using lists, it turns into a mess. I'll try this as an OOP program first.  
-
-
+FIXME Determine path forward with this. I have written a similar program in SDEV-120 using lists, it turns into a mess. I'll try this as an OOP program first.
+OOP proved to be too difficult at my current skill level, but I believe I was over-complicating the assignment initially anyway. A more direct approach was settled on.   
 '''
 last_name: str = ''
 first_name: str = ''
 gpa: float = 0 
 on_the_dl: float = float(3.5)
 on_the_hr: float = float(3.25)
-dl_message: str = ("This student has made the Dean's List.")
-hr_message: str =('This student is on the Honor Roll.')
-no_easy_way: str = ('Student did not make any academic list.')
-
+dl_message: str = ("This student has made the Dean's List.\n")
+hr_message: str =('This student is on the Honor Roll.\n')
+no_easy_way: str = ('Student did not make any academic list.\n')
 
 while True:
+
     last_name: str = input('Enter student last name: ')
+ 
     if last_name == 'ZZZ':
         break
+    else:
+         print(f'{last_name} accepted!\n')
+                
     first_name: str = input('Enter student first name: ')
-    gpa: float = float(input('Enter student gpa: '))
-
+    print(f'{first_name} accepted!')
+    while True:
+        try:
+            gpa: float = float(input('Enter student gpa: '))
+            print('GPA accepted!')
+            break
+        except ValueError:
+            print('Enter a float!')
+    # process user inputs
     if gpa >= on_the_dl:
         print(dl_message)
     elif gpa >= on_the_hr and gpa <= on_the_dl:
@@ -48,33 +58,8 @@ while True:
     else:
         print(no_easy_way)
 
-
 print('end_program')   
 
 
 
-
-'''
-# create class studentRecord with string fname, lname, and float gpa. 
-class studentRecord:
-    instance_incrementor: int = 0
-
-    def __int__(self):
-        self.lname = str(input('Enter student first name: '))
-        self.fname = str(input('Enter student first name: '))
-        self.gpa = float(input('Enter student gpa: '))
-        studentRecord.instance_incrementor += 1
-
-
-last_name = str(input('Enter student first name: '))
-first_name = str(input('Enter student first name: '))
-usr_in_gpa = float(input('Enter student gpa: '))
-
-lname = last_name
-fname = first_name
-gpa = usr_in_gpa
-
-
-print(studentRecord.__dict__)
-'''
 
