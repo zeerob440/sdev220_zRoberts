6660f937e2f975dd38bb4f8ebcb9ef54d8043053
zeerob440 <zeerob440@tutanota.com>
2026-01-27 11:20:20 -0500

    feat: added "given sorted arrays."

--- bin_search.py
+++ bin_search.py
@@ -9,4 +9,16 @@ Given a sorted array arr[] and an integer k, find the position(0-based indexing)
 
 Note: If multiple occurrences are there, please return the smallest index.
 
+Input: arr[] = [1, 2, 3, 4, 5], k = 4
+Output: 3
+Explanation: 4 appears at index 3.
+
+Input: arr[] = [11, 22, 33, 44, 55], k = 445
+Output: -1
+Explanation: 445 is not present.
+
+Input: arr[] = [1, 1, 1, 1, 2], k = 1
+Output: 0
+Explanation: 1 appears at index 0.
+
 '''
