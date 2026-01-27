0a2c9e297ad0f29700ddbb98c55ab1e4752f8609
zeerob440 <zeerob440@tutanota.com>
2026-01-27 11:03:42 -0500

    Feat: completed.

--- sort0_1.py
+++ sort0_1.py
@@ -21,6 +21,7 @@ Explanation: 0s, 1s and 2s are segregated into ascending order.
 
 # It makes soup sandwiches. 
 def soupSandwichMaker(arr):
+    # stores sandwich ingredients 
     sort_sequence: list = []
     # get 0's into sort_sequence
     for number in arr:
@@ -49,28 +50,8 @@ soupSandwichMaker(arr)
 arr:list = [0, 1, 1, 0, 1, 2, 1, 2, 0, 0, 0, 1] 
 
 soupSandwichMaker(arr)
-'''
-sort_sequence: list = []
-# get 0's into sort_sequence
-for number in arr:
-    if number == 0:
-        sort_sequence.append(number)
-        
-# get 1 into sort_sequence
-for number in arr:
-    if number == 1:
-        sort_sequence.append(number)
-       
-# get 2's into sort_sequence
-for number in arr:
-    if number == 2:
-        sort_sequence.append(number)
 
-# make arr == sort_sequence        
-arr = sort_sequence
-  
-print(arr) 
-'''
+
 
 
 
