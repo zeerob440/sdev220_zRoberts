58e0396690c4cebd9f4a9f3ee9bc65c7c05dc385
zeerob440 <zeerob440@tutanota.com>
2026-01-27 11:19:03 -0500

    Feat plate and specs declared.

---
+++ bin_search.py
@@ -0,0 +1,12 @@
+'''
+bin_search.py- it searches sorted arrays for a target value and returns the index in which the target is located.
+
+Proudly Engineered by Zachary Roberts 27 JAN 2026
+"It's like looking for a needle in a stack of needles."
+
+SPECIFICATIONS:
+Given a sorted array arr[] and an integer k, find the position(0-based indexing) at which k is present in the array using binary search. If k doesn't exist in arr[] return -1. 
+
+Note: If multiple occurrences are there, please return the smallest index.
+
+'''
