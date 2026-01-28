a5809f576b0c62f91f5ac10d6f661c139221da0a
zeerob440 <zeerob440@tutanota.com>
2026-01-28 11:59:07 -0500

    feat: spec, name and plate added. Vehicle class declared. Uncertain how to build inherence into a class, will have to wait for class tonight.

---
+++ v_oop_mod3.py
@@ -0,0 +1,35 @@
+'''
+v_oop_ mod3.py - is a oop intro program that declares the class Vehicle, the class Automobile inherits the traits of Vehicle
+
+Proudly Engineered by Zachary Roberts, 28 JAN 2026
+"Beats the heck out of walking there." 
+
+Write a Python app that has the following classes:
+A super class called Vehicle, which contains an attribute for vehicle type, such as car, truck, plane, boat, or a broomstick.
+A class called Automobile which will inherit the attributes from Vehicle and also contain the following attributes:
+year
+make
+model
+doors (2 or 4)
+roof (solid or sun roof).
+Write an app that will accept user input for a car. The app will store "car" into the vehicle type in your Vehicle super class. 
+The app will then ask the user for the year, make, model, doors, and type of roof and store thdata in the attributes above.
+The app will then output the data in an easy-to-read and understandable format, such as this:
+  Vehicle type: car
+  Year: 2022
+  Make: Toyota
+  Model: Corolla
+  Number of doors: 4
+  Type of roof: sun roof
+'''
+
+class  Vehicle:
+    def __int__(self, car, truck, plane, boat, broomstick):
+        self.car = car
+        self.truck = truck
+        self.plane = plane
+        self.boat = boat
+        self.broomstick = broomstick
+        
+
+
