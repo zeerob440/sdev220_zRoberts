c6313c7d65af758ed30eec11d37d7307eaa0c4da
zeerob440 <zeerob440@tutanota.com>
2026-02-18 20:00:29 -0500

    fixed: documentation

--- mod6juypterlab/fifteen_point_one.py
+++ mod6juypterlab/fifteen_point_one.py
@@ -3,7 +3,7 @@ import multiprocessing
 import time
 import os
 #15.1 Use multiprocessing to create three separate processes. Make each one wait a random number of seconds between zero and one, print the current time, and then exit.
-# could not get this program to run in Juypter Lab, but was able to run it in Pycharm and VS Code in .py files
+# could not get this program to run in Juypter Lab, but was able to run it in Pycharm and VS Code as .py files
 def random_interval_0_1_sec_current_time_printing_device_function():
         anchoring_time = time.ctime()
         range_random: float = random.uniform(0, 1)
