a8f2f70a881dfaa2962d3462c876524bf119e992
zeerob440 <zeerob440@tutanota.com>
2026-02-18 19:43:15 -0500

    debug: running my fuction in a for loop is probably not true concurrency. I'll move the for loop into random_interval_0_1_sec_current_time_printing_device_function():

--- mod6juypterlab/fifteen_point_one.py
+++ mod6juypterlab/fifteen_point_one.py
@@ -1,6 +1,7 @@
 import random
 import multiprocessing
 import time
+import os
 #15.1 Use multiprocessing to create three separate processes. Make each one wait a random number of seconds between zero and one, print the current time, and then exit.
 # could not get this program to run in Juypter Lab, but was able to run it in Pycharm and VS Code in .py files
 def random_interval_0_1_sec_current_time_printing_device_function():
@@ -8,6 +9,7 @@ def random_interval_0_1_sec_current_time_printing_device_function():
     time.sleep(range_random)
     anchoring_time = time.ctime()
     print(anchoring_time)
+    print("PID:", os.getpid(), "| Time:", anchoring_time)
 
 if __name__ == "__main__":
     for i in range(3):
